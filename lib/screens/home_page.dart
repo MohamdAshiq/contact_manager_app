d6423c4257ef074e16077df45d8309d78d07d4ad
Mohammed Ashiq <mohammedashiq836@gmail.com>
2025-08-18 19:15:14 +0530

    Created Initial Home Screen layout

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -23,6 +23,32 @@ class HomePage extends StatelessWidget {
           SizedBox(width: 7),
         ],
       ),
+      body: SafeArea(
+        child: ListView.separated(
+          separatorBuilder: (context, index) => Divider(),
+          padding: EdgeInsets.all(10),
+          itemCount: 10,
+          itemBuilder: (context, index) => ListTile(
+            onTap: () {},
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+            leading: Icon(Icons.account_circle, size: 42),
+            minLeadingWidth: 50,
+            contentPadding: EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+            title: Text("Name ${index + 1}"),
+            subtitle: Column(
+              spacing: 5,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("email${index + 1}@gmail.com"),
+                Text("9876543210"),
+              ],
+            ),
+            trailing: Icon(Icons.arrow_forward_ios_rounded, size: 20),
+          ),
+        ),
+      ),
     );
   }
 }
